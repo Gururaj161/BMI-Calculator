c3676984a71c126e85e6f66e24a5aced8b262523
Gururaja M <gururajkumar161@gmail.com>
2025-07-23 21:20:30 +0530

    removed print

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -27,7 +27,6 @@ class ViewController: UIViewController {
     
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         heightLabel.text = "\(String(format: "%.2f", sender.value)) m"
-        print("hi")
     }
     
     @IBAction func calculatePressedAction(_ sender: UIButton) {
